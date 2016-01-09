5fc847e2add409d8c983eaa059da4104d45742fc
stevenhao <stevenhao@gmail.com>
2016-01-09 16:19:34 -0500

    improve generator algorithm
    huge speed-up using better search algorithm
    - guess things with higher probability of success
      by identifying squares with the fewest candidate numbers

--- public/javascripts/generate.js
+++ public/javascripts/generate.js
@@ -15,6 +15,14 @@ function fillGrid(rows, cols, val) {
   return ret;
 }
 
+function fillGrid3(a, b, c, val) {
+  var ret = [];
+  for(var r = 0; r < a; ++r) {
+    ret.push(fillGrid(b, c, val));
+  }
+  return ret;
+}
+
 function rnd(n) {
   return Math.floor(n * Math.random());
 }
@@ -95,51 +103,136 @@ function hideOne() {
   }
 
   function uniqueSolution() {
-    var rows = fillGrid(9, 10, 0), cols = fillGrid(9, 10, 0), squares = fillGrid(9, 10, 0);
-    function put(x, y, val) {
-      if (rows[x][val] || cols[y][val] || squares[sqs[x][y]][val]) {
-        return false;
+    function countSols2() {
+      var used = fillGrid3(9, 9, 10, 0);
+      var done = fillGrid(9, 9, 0);
+      var choices = fillGrid(9, 9, 9);
+      function inc(x, y, val) {
+        if (used[x][y][val] == 0) {
+          --choices[x][y];
+        }
+        ++used[x][y][val];
       }
-      rows[x][val] = cols[y][val] = squares[sqs[x][y]][val] = 1;
-      return true;
-    }
-
-    function unput(x, y, val) {
-      rows[x][val] = cols[y][val] = squares[sqs[x][y]][val] = 0;
-    }
-
-    var cnt = 0;
-    function dfs(x, y) { // returns # sols
-      var nx = x, ny = y + 1;
-      if (ny == 9) {
-        ny = 0;
-        ++nx;
-      }
-      if (nx == 9) {
-        ++cnt;
-        return;
-      }
-      if (public[x][y]) {
-        return dfs(nx, ny);
-      }
-
-      for (var val = 1; val <= 9; ++val) {
-        if (put(x, y, val)) {
-          dfs(nx, ny);
-          unput(x, y, val);
+      function dec(x, y, val) {
+        --used[x][y][val];
+        if (used[x][y][val] == 0) {
+          ++choices[x][y];
         }
       }
+      function put(x, y, val) {
+        for(var i = 0; i < 9; ++i) {
+          inc(i, y, val);
+          inc(x, i, val);
+        }
+        var sx = x - x % 3, sy = y - y % 3;
+        for (var i = 0; i < 3; ++i) {
+          for (var j = 0; j < 3; ++j) {
+            inc(sx + i, sy + j, val);
+          }
+        }
+        done[x][y] = val;
+      }
+      function unput(x, y, val) {
+        for(var i = 0; i < 9; ++i) {
+          dec(i, y, val);
+          dec(x, i, val);
+        }
+        var sx = x - x % 3, sy = y - y % 3;
+        for (var i = 0; i < 3; ++i) {
+          for (var j = 0; j < 3; ++j) {
+            dec(sx + i, sy + j, val);
+          }
+        }
+        done[x][y] = 0;
+      }
+      var cnt = 0;
+      function dfs2() {
+        var mx = -1, my = -1, mn = 10;
+        for(var x = 0; x < 9; ++x) {
+          for(var y = 0; y < 9; ++y) {
+            if (!done[x][y]) {
+              if (choices[x][y] < mn) {
+                mn = choices[x][y];
+                mx = x;
+                my = y;
+              }
+            }
+          }
+        }
+
+        if (mn == 0) {}
+        else if (mn == 10) ++cnt;
+        else {
+          for (var v = 1; v <= 9; ++v) {
+            if (used[mx][my][v] == 0) {
+              put(mx, my, v);
+              dfs2();
+              unput(mx, my, v);
+              if (cnt >= 2) return;
+            }
+          }
+        }
+      }
+      for(var i = 0; i < 9; ++i) {
+        for(var j = 0; j < 9; ++j) {
+          if (public[i][j]) {
+            put(i, j, private[i][j]);
+          }
+        }
+      }
+      dfs2();
+      return cnt;
     }
 
-    for(var x = 0; x < 9; ++x) {
-      for(var y = 0; y < 9; ++y) {
+    function countSols() {
+      var rows = fillGrid(9, 10, 0), cols = fillGrid(9, 10, 0), squares = fillGrid(9, 10, 0);
+      function put(x, y, val) {
+        if (rows[x][val] || cols[y][val] || squares[sqs[x][y]][val]) {
+          return false;
+        }
+        rows[x][val] = cols[y][val] = squares[sqs[x][y]][val] = 1;
+        return true;
+      }
+
+      function unput(x, y, val) {
+        rows[x][val] = cols[y][val] = squares[sqs[x][y]][val] = 0;
+      }
+
+      var cnt = 0;
+      function dfs(x, y) { // returns # sols
+        var nx = x, ny = y + 1;
+        if (ny == 9) {
+          ny = 0;
+          ++nx;
+        }
+        if (nx == 9) {
+          ++cnt;
+          return;
+        }
         if (public[x][y]) {
-          put(x, y, private[x][y]);
+          return dfs(nx, ny);
+        }
+
+        for (var val = 1; val <= 9; ++val) {
+          if (put(x, y, val)) {
+            dfs(nx, ny);
+            unput(x, y, val);
+          }
         }
       }
+
+      for(var x = 0; x < 9; ++x) {
+        for(var y = 0; y < 9; ++y) {
+          if (public[x][y]) {
+            put(x, y, private[x][y]);
+          }
+        }
+      }
+      dfs(0, 0);
+      return cnt;
     }
-    dfs(0, 0);
-    print('numsols=', cnt);
+    var cnt = countSols2();
+    print('cnt=', cnt);
     return cnt == 1;
   }
 
@@ -180,7 +273,7 @@ function unhideOne() {
 }
 
 var difficulty = null;
-var sliderValue = 28;
+var sliderValue = 40;
 var running = false;
 
 function changeDifficulty() {
